6d74e1b405e1e481799646681e376f70a0a4ae17
Nikolas <nikolas217@mail.ru>
2024-03-27 00:44:55 +0500

    Style fixes

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Epic extends Task {
 
     private ArrayList<Integer> subtaskIDs = new ArrayList<>();
+
     public Epic(String title, String description) {
         super(title, description);
     }
