7e6c59fdc5fd055ae90a706c55b44874105215cd
Nikolas <nikolas217@mail.ru>
2024-05-17 22:49:56 +0500

    Стилистика

--- src/taskmanager/InMemoryTaskManager.java
+++ src/taskmanager/InMemoryTaskManager.java
@@ -11,7 +11,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class InMemoryTaskManager implements TaskManager {
     protected int counter = 0;
